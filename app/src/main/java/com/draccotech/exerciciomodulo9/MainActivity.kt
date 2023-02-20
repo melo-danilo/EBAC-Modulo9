14092cfed1685078794575725de9466b4a124a3a
Danilo Melo <danilofr.melo@gmail.com>
2023-02-20 15:45:32 -0300

    meu cartao de visita

--- app/src/main/java/com/draccotech/exerciciomodulo9/MainActivity.kt
+++ app/src/main/java/com/draccotech/exerciciomodulo9/MainActivity.kt
@@ -7,5 +7,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        /*
+            Utilizando o que aprendemos na aula e sua criatividade
+            desenvolva um cartao de visita
+
+            O cartao deve conter pelo menos:
+                * Logo da Empresa
+                * Nome
+                * Email
+                * Telefone
+                * Endereco
+         */
     }
 }
